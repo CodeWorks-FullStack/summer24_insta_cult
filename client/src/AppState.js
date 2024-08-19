0d5d25384940dc26490cc79df5440946e5f6efa6
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-19 11:41:21 -0600

    added cults array to appstate

--- client/src/AppState.js
+++ client/src/AppState.js
@@ -1,4 +1,5 @@
 import { reactive } from 'vue'
+import { Cult } from './models/Cult.js'
 
 // NOTE AppState is a reactive object to contain app level data
 export const AppState = reactive({
@@ -6,6 +7,7 @@ export const AppState = reactive({
   identity: null,
   /** @type {import('./models/Account.js').Account} user info from the database*/
   account: null,
-  banans: ["apple", "orange", "banana"]
+  /** @type {Cult[]}*/
+  cults: []
 })
 
