db3d0226078039746532e506e5b4a2d3c31e793b
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-19 11:04:34 -0600

    home page looks pretty good

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -18,11 +18,9 @@ function toggleTheme() {
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-sm navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-sm px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
-      <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="/img/cw-logo.png" height="45" />
-      </div>
+      <h1 class="megrim-font text-light">InstaCult</h1>
     </router-link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText"
       aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -31,9 +29,7 @@ function toggleTheme() {
     <div class="collapse navbar-collapse" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
+
         </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
--- client/src/pages/HomePage.vue
+++ client/src/pages/HomePage.vue
@@ -3,35 +3,31 @@
 </script>
 
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 card align-items-center shadow rounded elevation-3">
-      <img src="@/assets/img/cw-circle-logo.png" alt="CodeWorks Logo"
-        class="rounded-circle">
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
-  </div>
+  <section class="">
+    <button class="btn btn-outline-danger text-light fw-bold megrim-font fs-1">
+      Join A Cult?
+    </button>
+    <button class="btn btn-outline-danger text-light fw-bold megrim-font fs-1">
+      Start A Cult?
+    </button>
+  </section>
 </template>
 
-<style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+<style lang="scss">
+section {
+  min-height: var(--main-height);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 2rem;
+}
 
-  .home-card {
-    width: clamp(500px, 50vw, 100%);
+body {
+  background-image: url(https://images.unsplash.com/photo-1483982258113-b72862e6cff6?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);
+  background-size: cover;
+}
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
-  }
+#app {
+  backdrop-filter: grayscale(1);
 }
 </style>
