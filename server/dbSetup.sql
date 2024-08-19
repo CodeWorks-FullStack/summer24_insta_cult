8243f9b1e39c4e0ae8134d5b3536f17ec2331c10
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-19 09:46:37 -0600

    made fake data

--- server/dbSetup.sql
+++ server/dbSetup.sql
@@ -20,3 +20,19 @@ CREATE TABLE
     leaderId VARCHAR(255) NOT NULL,
     FOREIGN KEY (leaderId) REFERENCES accounts (id) ON DELETE CASCADE
   );
+
+SELECT
+  *
+FROM
+  accounts;
+
+INSERT INTO
+  cults (name, fee, description, coverImg, leaderId)
+VALUES
+  (
+    "The Cultergeists",
+    0,
+    "We move things around on the lectern. We unplug cords. Evan is maybe a member.",
+    "https://plus.unsplash.com/premium_photo-1683121243798-8f0eb5b0e03b?q=80&w=2340&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "66bb8ddd637da0ef8d71e1e7"
+  );
