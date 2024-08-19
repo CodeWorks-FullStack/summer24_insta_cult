13b43d50146daed6bb7ff02ca59d4d293d10db59
Jeremy Fowler <jembyfowler@gmail.com>
2024-08-19 09:30:40 -0600

    init

---
+++ server/Models/Account.cs
@@ -0,0 +1,9 @@
+namespace insta_cult.Models;
+
+public class Account
+{
+  public string Id { get; set; }
+  public string Name { get; set; }
+  public string Email { get; set; }
+  public string Picture { get; set; }
+}
